06b794fe321bf984d5e43501231eb9fc2b145fe0
RyanSpringer1500 <79956601+RyanSpringer1500@users.noreply.github.com>
2021-05-30 18:40:53 -0500

    AddedPrototype1Runthrough

---
+++ New Unity Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public float turnspeed;
+    public float forwardInput;
+    public float horizontalInput;
+      
+
+    // Update is called once per frame
+    void Update()
+    {
+        forwardInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal");
+        //move forward
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        transform.Rotate(Vector3.up, turnspeed * Time.deltaTime * horizontalInput);
+    }
+}
