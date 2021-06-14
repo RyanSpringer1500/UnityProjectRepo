45a3b324f61d06556390ceb80bb5263a84393f3d
RyanSpringer1500 <79956601+RyanSpringer1500@users.noreply.github.com>
2021-06-13 23:14:00 -0500

    setting ground work for player's actions

---
+++ Project 1/Assets/Scripts/correctAnswerMove.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class correctAnswerMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // display math problems 
+        // display player imput text
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //while(true)
+        // player imputs answer and then checks the solutions by compare the value.
+        //if(correct)
+        // player moves in certain direction = to solution.
+        //else
+        // display text incorrect
+        // if end loop when end goal triggers: meaning player wins 
+        // if end loop when msitakes were made by player: meaning game over
+        
+    }
+}
