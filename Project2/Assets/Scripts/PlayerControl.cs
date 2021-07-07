7c96e0d9aae152b88f3294b81b30634c1ab59dad
RyanSpringer1500 <79956601+RyanSpringer1500@users.noreply.github.com>
2021-07-06 23:36:11 -0500

    project2

---
+++ Project2/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,41 @@
+﻿/* * (Ryan Springer) 
+ * * (project2) 
+ * * (control player) */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    public float maxSpeed = 7;
+    public float jumpTakeOffSpeed = 7;
+
+  
+
+    
+
+    void ComputeVelocity()
+    {
+        Vector2 move = Vector2.zero;
+
+        move.x = Input.GetAxis("Horizontal");
+
+        if (Input.GetButtonDown("Jump") && grounded)
+        {
+            velocity.y = jumpTakeOffSpeed;
+        }
+        else if (Input.GetButtonUp("Jump"))
+        {
+            if (velocity.y > 0)
+            {
+                velocity.y = velocity.y * 0.5f;
+            }
+        }
+
+       
+
+       
+
+        targetVelocity = move * maxSpeed;
+    }
