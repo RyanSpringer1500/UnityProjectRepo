70c4b941bdee096446ba40e72b540aebcbd98eb3
RyanSpringer1500 <79956601+RyanSpringer1500@users.noreply.github.com>
2021-07-18 22:43:49 -0500

    creating challenge4

---
+++ challenge4/Assets/Challenge 4/Scripts/DestoryText.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class DestoryText : MonoBehaviour
+{
+    public Text textbox;
+    // Update is called once per frame
+    void Update()
+    {
+       if(Input.GetKeyDown(KeyCode.RightShift))
+        {
+            textbox.text = " ";
+        }
+    }
+}
---
+++ challenge4/Assets/Challenge 4/Scripts/ScoreManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ScoreManager : MonoBehaviour
+{
+    public static bool gameOver = false;
+    public static bool won = false;
+
+    public Text textbox;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameOver = false;
+        won = false;
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!gameOver)
+        {
+            textbox.text = "Wave:  " + SpawnManagerX.wavenumber;
+        }
+        if (SpawnManagerX.wavenumber >= 10)
+        {
+            won = true;
+            gameOver = true;
+        }
+        if (gameOver)
+        {
+            if (won)
+            {
+                textbox.text = "You Won! \n Press R to Try Again";
+            }
+            else
+            {
+                textbox.text = "You Lose!  \n Press R to Try Again";
+            }
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
+            }
+        }
+
+    }
+
+}
