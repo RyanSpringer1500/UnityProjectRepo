ccfa410ab1980e5926ebe26fb8fc3e2a57b4b80b
RyanSpringer1500 <79956601+RyanSpringer1500@users.noreply.github.com>
2021-07-25 21:41:46 -0500

    creating challenge5

---
+++ challenge5/Assets/Challenge 5/Scripts/DestroyObjectX.cs
@@ -0,0 +1,18 @@
+﻿/* * (Ryan Springer) * 
+ * (Assignment8) * 
+ * (destorys in game objects) */
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyObjectX : MonoBehaviour
+{
+    void Start()
+    {
+        Destroy(gameObject, 2); // destroy particle after 2 seconds
+    }
+
+
+}
